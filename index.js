44a142882aea3d57d376d4694875e37e2ced3c29
Zack Mulgrew <zack@zackthehuman.com>
2016-08-28 00:50:54 -0700

    Refactor, move code to lib/

--- index.js
+++ index.js
@@ -1,32 +1,14 @@
 /* jshint node: true */
 'use strict';
 
-var Funnel = require('broccoli-funnel');
-var stew = require('broccoli-stew');
+var Addon      = require('./lib/i18n-addon');
+var Funnel     = require('broccoli-funnel');
+var stew       = require('broccoli-stew');
 var mergeTrees = require('broccoli-merge-trees');
-var path = require('path');
-var existsSync = require('exists-sync');
-var uniq = require('lodash.uniq');
-var result = require('lodash.result');
+var path       = require('path');
 
-module.exports = {
+module.exports = Addon.extend({
   name: 'ember-template-i18n',
-
-  isNestedAddon: function() {
-    return !!this.parent.parent;
-  },
-
-  getParentName: function() {
-    return result(this.parent, 'name');
-  },
-
-  // preprocessTree: function(name, tree) {
-  //   if (name === 'template') {
-  //     return stew.log(tree, { output: 'tree', label: 'preprocessTree - template' });
-  //   }
-
-  //   return tree;
-  // },
 
   setupPreprocessorRegistry: function(type, registry) {
     if (type === 'parent') {
@@ -42,7 +24,7 @@ module.exports = {
         _addon: this,
 
         toTree: function(tree) {
-          this._addon._translationTemplates = tree;
+          this._addon._treeForExtraction = tree; // tree to extract i18n strings from
 
           return stew.log(tree, {
             output: 'tree',
@@ -55,107 +37,29 @@ module.exports = {
 
   treeForPublic: function(tree) {
     var publicTree = this._super.treeForPublic.apply(this, arguments);
-    var trees = [publicTree, this._translationTemplates, this._treeForTranslation()].filter(Boolean);
+    var trees = [publicTree];
 
     if (!this.isNestedAddon()) {
-      // When this addon is the child of an app, it has to find any other instances
-      // of itself (which were included in other addons) and get their trees for translation.
-      var i18nAddons = collectAddons(this.parent, result(this, 'name'));
+      // Push "my own" translation tree
+      trees.push(this._treeForTranslation());
+
+      // When this addon is the child of an app, it has to find any other
+      // instances of itself (which were included in other addons) and get
+      // their trees for translation.
       var i18nTrees = mergeTrees(this._collectTranslationTrees());
 
-      trees.push(new Funnel(i18nTrees, {
-        destDir: 'properties'
-      }));
+      // Push "other addon's" translation trees
+      trees.push(i18nTrees);
     }
+
+    trees = trees.filter(Boolean);
 
     if (trees.length) {
       return new Funnel(mergeTrees(trees), {
-        destDir: 'i18n'
+        destDir: path.join('i18n', 'properties')
       });
     }
 
     return publicTree;
-  },
-
-  // treeForApp: function(tree) {
-  //   var appTree = this._super.treeForApp.apply(this, arguments);
-
-  //   if (!this.isNestedAddon()) {
-  //     return stew.log(appTree, { output: 'tree', label: 'app tree' });
-  //   }
-
-  //   return appTree;
-  // },
-
-  /**
-   * The tree containing translated .properties files.
-   */
-  _treeForTranslation: function() {
-    var i18nPath = path.join(this.parent.root, 'i18n');
-    var parentName = this.getParentName();
-    var ADDON_TEMPLATES = path.join('addon', 'templates');
-
-    if (existsSync(i18nPath)) {
-      console.log('Found "i18n/" directory in "' + this.parent.root + '"');
-      return new Funnel(i18nPath, {
-        getDestinationPath: function(relativePath) {
-          // Move "on-disk" files to their "ember addon" path.
-          //
-          // From:
-          //   addon/templates/components/example-addon-component_en_US.properties
-          //
-          // To:
-          //   modules/<addon-name>/templates/components/example-addon-component_en_US.properties
-          //
-          if (relativePath.indexOf(ADDON_TEMPLATES) === 0) {
-            return path.join(
-              'modules',
-              parentName,
-              relativePath.replace(ADDON_TEMPLATES, 'templates')
-            );
-          }
-
-          return relativePath;
-        }
-      });
-    } else {
-      console.log('No "i18n/" directory found in "' + this.parent.root + '"');
-    }
-  },
-
-  _collectTranslationTrees: function() {
-    var trees = uniq(collectAddons(this.parent, result(this, 'name')));
-
-    return trees.map(function(addon) {
-      var trees = [
-        addon._translationTemplates, // templates that need to be translated
-        addon._treeForTranslation()  // translated .properties files
-      ].filter(Boolean);
-
-      console.log(trees.length);
-
-      return mergeTrees(trees);
-    });
   }
-};
-
-
-function collectAddons(owner, name, ret) {
-  ret = ret || [];
-
-  var notSelfAddons = owner.addons.filter(function(addon) {
-    return result(addon, 'name') !== name;
-  });
-
-  notSelfAddons.forEach(function(addon) {
-    addon.addons.forEach(function(childAddon) {
-      if (result(childAddon, 'name') === name) {
-        ret.push(childAddon);
-      }
-
-      collectAddons(addon, name, ret);
-    });
-  });
-
-  return ret;
-}
+});
