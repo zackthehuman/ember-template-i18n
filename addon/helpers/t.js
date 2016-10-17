7d3110f9b0ba51c88ccca066e6d55f3a6f80e18a
Zack Mulgrew <zack@zackthehuman.com>
2016-10-16 23:48:18 -0700

    Implement t helper that uses string bundle as source

--- addon/helpers/t.js
+++ addon/helpers/t.js
@@ -1,8 +1,29 @@
 import Ember from 'ember';
 
+const {
+  get,
+  observer
+} = Ember;
+
 export function translate(params) {
   const key = params[0];
-  return `t(${key})`;
+  const locale = params[1];
+  const ns = params[2];
+  const strings = require('ember-template-i18n/utils/strings')['default'];
+  return get(strings, `${locale}.${ns}.${key}`);
 }
 
-export default Ember.Helper.helper(translate);
+export default Ember.Helper.extend({
+  i18n: Ember.inject.service('i18n'),
+
+  compute: function(params) {
+    const i18n = get(this, 'i18n');
+    const locale = get(i18n, 'interfaceLocale');
+
+    return i18n.getMessage(locale, params[1], params[0]);
+  },
+
+  interfaceLocaleChanged: observer('i18n.interfaceLocale', function() {
+    this.recompute();
+  })
+});
